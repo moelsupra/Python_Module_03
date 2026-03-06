7ceab192925d23c04f0e5b2722ea5a52f149ffef
moelsupra <medamineelm01@gmail.com>
2026-03-06 17:45:45 +0000

    updated

--- ex1/ft_score_analytics.py
+++ ex1/ft_score_analytics.py
@@ -5,16 +5,23 @@ def main() -> None:
     print("=== Player Score Analytics ===")
     if len(sys.argv) > 1:
         try:
-            scores = [int(arg) for arg in sys.argv[1:]]
+            scores = []
+            for arg in sys.argv[1:]:
+                scores.append(int(arg))
+            length = len(scores)
+            total = sum(scores)
+            high = max(scores)
+            low = min(scores)
             print(f"Scores processed: {scores}")
-            print(f"Total players: {len(scores)}")
-            print(f"Total score: {sum(scores)}")
-            print(f"Average score: {sum(scores) / len(scores)}")
-            print(f"High score: {max(scores)}")
-            print(f"Low score: {min(scores)}")
-            print(f"Score range: {max(scores) - min(scores)}")
+            print(f"Total players: {length}")
+            print(f"Total score: {total}")
+            print(f"Average score: {total / length}")
+            print(f"High score: {high}")
+            print(f"Low score: {low}")
+            print(f"Score range: {high - low}")
         except ValueError:
-            print("That's not a number!")
+            print(f"oops, I typed {arg} instead of a number "
+                  f"for example '1000'")
     else:
         print("No scores provided. Usage: "
               "python3 ft_score_analytics.py <score1> <score2> ..."
